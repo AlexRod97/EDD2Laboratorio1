1056ebf8e109b2deab914dff0d18cd275515a723
AlexRod97 <rodriguezalex86@hotmail.com>
2018-09-19 01:44:41 -0600

    Adding method to navigate files

--- app/src/main/java/com/e/edd2laboratorio1/MainActivity.java
+++ app/src/main/java/com/e/edd2laboratorio1/MainActivity.java
@@ -1,13 +1,99 @@
 package com.e.edd2laboratorio1;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenFile;
+    private static final int PERMISSION_REQUEST_STORAGE = 1000;
+    private static final int READ_REQUEST_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
+        }
+        btnOpenFile = (Button)findViewById(R.id.btnOpenFile);
+
+        btnOpenFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileSearch();
+            }
+        });
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] granResults) {
+        if (requestCode == PERMISSION_REQUEST_STORAGE) {
+            if(granResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private String readText(String input) {
+        File file = new File(input);
+        StringBuilder text  = new StringBuilder();
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine()) != null) {
+                text.append(line);
+                text.append("\n");
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+
+    private void fileSearch() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected  void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if(data != null) {
+                Uri uri = data.getData();
+                String path = uri.getPath();
+                path = path.substring(path.indexOf(":")+1);
+                Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
     }
 }

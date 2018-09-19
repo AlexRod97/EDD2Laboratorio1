9c236e1cd5ea2f13a1f20a3f47aa1ec18bab16da
AlexRod97 <rodriguezalex86@hotmail.com>
2018-09-19 02:21:10 -0600

    Added methods to save files

--- app/src/main/java/com/e/edd2laboratorio1/MainActivity.java
+++ app/src/main/java/com/e/edd2laboratorio1/MainActivity.java
@@ -7,22 +7,28 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnOpenFile;
+    Button button;
+    EditText editText;
     private static final int PERMISSION_REQUEST_STORAGE = 1000;
     private static final int READ_REQUEST_CODE = 42;
 
@@ -35,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
 
             requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
         }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
+        }
+
+        editText = (EditText)findViewById(R.id.editText);
+
+        button = (Button)findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = editText.getText().toString();
+                String fileName = "Trial";
+                SaveText(fileName, content);
+            }
+        });
+
         btnOpenFile = (Button)findViewById(R.id.btnOpenFile);
 
         btnOpenFile.setOnClickListener(new View.OnClickListener() {
@@ -45,14 +70,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] granResults) {
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == PERMISSION_REQUEST_STORAGE) {
-            if(granResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show();
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permiso de lectura concedido", Toast.LENGTH_SHORT).show();
             }
             else {
-                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Permiso de lectura denegado", Toast.LENGTH_SHORT).show();
                 finish();
+            }
+
+            switch (requestCode) {
+                case 1000:
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permiso de escritura concedido", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(this, "Permiso de lectura denegado", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
             }
         }
     }
@@ -74,6 +110,22 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return text.toString();
+    }
+
+    private void SaveText(String filename, String content) {
+        String fileName = filename + ".txt";
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+
+        try {
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(file);
+            fos.write(content.getBytes());
+            fos.close();
+            Toast.makeText(this, "Archivo guardado", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error al almacenar el archivo", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void fileSearch() {
